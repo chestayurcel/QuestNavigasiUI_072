ec86081142ce4fe63827ca1e6cb092b43efff55e
Yurcell <chestayurcel@gmail.com>
2025-11-06 13:13:08 +0700

    menambahkan dependensi material3 dan menambahkan import modifier

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -2,16 +2,18 @@
 
 package com.example.praktikum5.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
-    Scaffold (modifier=Modifier,
+    Scaffold (modifier= Modifier,
         {
             TopAppBar(
 
