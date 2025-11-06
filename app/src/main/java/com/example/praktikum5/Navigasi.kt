0bd99a51aa441a9464bd93b5fc4f460556ddc460
Yurcell <chestayurcel@gmail.com>
2025-11-06 12:14:38 +0700

    membuat navhost dan scaffold untuk navigasi antar layar

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -1,7 +1,38 @@
 package com.example.praktikum5
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
 enum class Navigasi {
     Formulirku,
     Detail
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian (
+                    //pilihan JK = JenisK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
+}
